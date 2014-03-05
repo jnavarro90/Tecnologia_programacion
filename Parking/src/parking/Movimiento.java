cdd0db7afc895e8ef6990c5ca817ba878ae95cb5
Javi <jnavarroa90@gmail.com>
2014-03-05 15:23:44 +0100

    Primera carga en repositorio

---
+++ Parking/src/parking/Movimiento.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parking;
+
+/**
+ *
+ * @author javi
+ */
+public class Movimiento {
+    
+}
---
+++ Parking/src/parking/Vehiculo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parking;
+
+/**
+ *
+ * @author javi
+ */
+public class Vehiculo {
+    String matricula;
+    int tiempoEntrada;
+    Ticket tickets[];
+}
